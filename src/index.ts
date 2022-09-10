932f3796a904b98a7b1d0e3891f180856a04fd0f
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-09-10 11:46:43 -0300

    re-ident code

--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,13 @@
 
 function shuffle<T>(array: T[]): T[] {
   const newArray = array;
-
   let currentIndex = newArray.length;
 
   while (currentIndex !== 0) {
     const randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
-
     [newArray[currentIndex], newArray[randomIndex]] = [newArray[randomIndex], newArray[currentIndex]];
   }
-
   return newArray;
 }
 
